63204558b403e796f904fcb0b62406a6ea5094c3
Eike Wichern <e.wichern@outlook.de>
2015-06-25 20:31:46 +0200

    Vererbung nochmal gefixt

--- Vererbung/Vererbung.java
+++ Vererbung/Vererbung.java
@@ -21,39 +21,31 @@ public class Vererbung {
 		
 		Out.println();
 		
-		Person a2 = new Person();
-		Person b2 = new Person();
+		Person a2 = a;
+		Person b2 = b;
 		
 		for (int i = 0; i < 10; i++) {
 			
 			Gen2_1.printPerson();
 			Gen2_2.printPerson();
+			
+			while (Gen2_1.Geschlecht == Gen2_2.Geschlecht) {
+				
+				if (Math.random() >= 0.5) {
+					Gen2_1 = Person.Fortpflanzen(a2, b2);
+					Gen2_1.printPerson();
+				} else {
+					Gen2_2 = Person.Fortpflanzen(a2, b2);
+					Gen2_2.printPerson();
+				}
+				
+			}
+			
 			a2=Gen2_1;
 			b2=Gen2_2;
 			Gen2_1 = Person.Fortpflanzen(a2, b2);
 			Gen2_2 = Person.Fortpflanzen(a2, b2);
 			
-			while (Gen2_1 == null) {
-				
-				if (a2.Geschlecht == true) {		//wenn beide Eltern Männer
-					Gen2_1 = Person.Fortpflanzen(a, b2);
-				} else {
-					Gen2_1 = Person.Fortpflanzen(a2, b);
-				}
-				
-				Gen2_1.printPerson();
-			}
-			
-			while (Gen2_2 == null) {
-
-				if (a2.Geschlecht == true) {		//wenn beide Eltern Männer
-					Gen2_2 = Person.Fortpflanzen(a, b2);
-				} else {
-					Gen2_2 = Person.Fortpflanzen(a2, b);
-				}
-				
-				Gen2_2.printPerson();
-			}
 			Out.println();
 			
 
